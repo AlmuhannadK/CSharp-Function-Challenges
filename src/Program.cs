df25a2463a55a749277e2da3666f15532f746848
Almuhannad <almuhannad.khalid@gmail.com>
2024-03-20 13:12:03 +0300

    added Guessing Gam (Challenge 3)

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
+
+using System.Text;
 
 namespace FunctionChallenges
 {
@@ -8,13 +9,51 @@ namespace FunctionChallenges
         static void Main(string[] args)
         {
             // Challenge 1: String and Number Processor
-            Console.WriteLine("Challenge 1: String and Number Processor");
-            StringNumberProcessor("Hello", 100, 200, "World"); // Expected outcome: "Hello World; 300"
+            // Console.WriteLine("Challenge 1: String and Number Processor");
 
-            void StringNumberProcessor()
-            {
+            // dynamic test = "hello";
 
-            }
+            // //Console.WriteLine($"Type of this variable is: {}");
+            // if (typeof(test) is string)
+            // {
+
+            //     Console.WriteLine("string");
+            // }
+
+            // static dynamic StringNumberProcessor(params dynamic[] something)
+            // {
+            //     if (typeof(something) is string)
+            //     {
+            //         StringBuilder sb = new StringBuilder();
+            //         for (int i = 0; i < something.Length; i++)
+            //         {
+            //             sb.Append(something[i]);
+            //             string resultString = sb.ToString();
+            //             return resultString;
+            //         }
+            //     }
+            //     else if (typeof(something) is int)
+            //     {
+            //         int total = 0;
+            //         for (int i = 0; i < something.Length; i++)
+            //         {
+            //             total += something[i];
+            //         }
+            //         return total;
+            //     }
+            //     else
+            //     {
+            //         return null;
+            //     }
+            // }
+
+
+
+
+
+
+
+
 
             /*
             /// Challenge 2: Object Swapper
@@ -39,12 +78,56 @@ namespace FunctionChallenges
             Console.WriteLine($"Strings: {str1}, {str2}");
             */
 
-            /*
+
             /// Challenge 3: Guessing Game
             Console.WriteLine("\nChallenge 3: Guessing Game");
-            /// Uncomment to test the GuessingGame method
-            /// GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
-            */
+
+            void GuessingGame()
+            {
+                Random random = new Random();
+                bool playAgain = true;
+                int min = 1;
+                int max = 50;
+                int guess;
+                int number;
+                int attempts;
+
+                while (playAgain)
+                {
+                    guess = 0;
+                    attempts = 0;
+                    number = random.Next(min, max + 1);
+
+                    while (guess != number)
+                    {
+                        Console.WriteLine("Please enter a number between 1 - 100 :");
+                        guess = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"You guesses: {guess}");
+                        if (guess > number)
+                        {
+                            Console.WriteLine($"{guess} is ABOVE the number");
+                            attempts++;
+
+                        }
+                        else if (guess < number)
+                        {
+                            Console.WriteLine($"{guess} is BELOW the number");
+                            attempts++;
+                        }
+                        if (guess == number)
+                        {
+                            Console.WriteLine($"Wow you guessed it right! the number was {number}. Attempts made: {attempts}");
+                            playAgain = false;
+                        }
+                    }
+
+
+
+                }
+            }
+
+            GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+
 
 
             /*
