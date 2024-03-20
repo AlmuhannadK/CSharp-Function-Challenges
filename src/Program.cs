cc729ce27d14001e190c8cc363be64878664dfc4
Almuhannad <almuhannad.khalid@gmail.com>
2024-03-20 10:24:27 +0300

    pr commit

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FunctionChallenges
 {
@@ -10,12 +11,20 @@ namespace FunctionChallenges
             Console.WriteLine("Challenge 1: String and Number Processor");
             StringNumberProcessor("Hello", 100, 200, "World"); // Expected outcome: "Hello World; 300"
 
-            // Challenge 2: Object Swapper
+            void StringNumberProcessor()
+            {
+
+            }
+
+            /*
+            /// Challenge 2: Object Swapper
             Console.WriteLine("\nChallenge 2: Object Swapper");
             int num1 = 25, num2 = 30;
             int num 3 = 10, num4 = 30;
             string str1 = "HelloWorld", str2 = "Programming";
             string str3 = "Hi", str4 = "Programming";
+            
+
                           
             SwapObjects(ref num1, ref num2); // Expected outcome: num1 = 30, num2 = 25  
             SwapObjects(ref num3, ref num4); // Error: Value must be more than 18
@@ -28,17 +37,23 @@ namespace FunctionChallenges
 
             Console.WriteLine($"Numbers: {num1}, {num2}");
             Console.WriteLine($"Strings: {str1}, {str2}");
+            */
 
-            // Challenge 3: Guessing Game
+            /*
+            /// Challenge 3: Guessing Game
             Console.WriteLine("\nChallenge 3: Guessing Game");
-            // Uncomment to test the GuessingGame method
-            // GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+            /// Uncomment to test the GuessingGame method
+            /// GuessingGame(); // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+            */
 
-            // Challenge 4: Simple Word Reversal
+
+            /*
+            /// Challenge 4: Simple Word Reversal
             Console.WriteLine("\nChallenge 4: Simple Word Reversal");
             string sentence = "This is the original sentence!";
             string reversed = ReverseWords(sentence);
             Console.WriteLine(reversed); // Expected outcome: "sihT si eht lanigiro !ecnetnes"
+            */
         }
     }
 }
